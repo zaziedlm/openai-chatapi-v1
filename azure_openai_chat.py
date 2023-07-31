a629c634e317df50cc023b2257fdcdf562349860
Shuichi Kataoka <shu01k9@gmail.com>
2023-07-31 10:49:52 +0900

    Azure OpenAI向け環境キーワード変数化

--- azure_openai_chat.py
+++ azure_openai_chat.py
@@ -3,15 +3,15 @@ import os
 import openai
 import openaiapikey
 
-openai.api_type = "azure"
-openai.api_base = "https://gptsample01.openai.azure.com/"
-openai.api_version = "2023-03-15-preview"
+openai.api_type = openaiapikey.azure_api_type
+openai.api_base = openaiapikey.azure_api_base
+openai.api_version = openaiapikey.azure_api_version
 openai.api_key = openaiapikey.azure_api_key
 
 response = openai.ChatCompletion.create(
-  engine="gpt-35-turbo-0713",
+  engine=openaiapikey.azure_api_engine,
   messages = [{"role":"system","content":"You are an AI assistant that helps people find information."},
-              {"role":"user","content":"日本で一番高い山は何ですか？"}],
+              {"role":"user","content":"日本で一番高い山は何ですか、二番目に高い山も教えてください？"}],
   temperature=0.7,
   max_tokens=800,
   top_p=0.95,
---
+++ openaiapikey_template.py
@@ -0,0 +1,11 @@
+# openaiapikey.py template file
+# 
+# OpenAI API KEY
+secret_api_key ='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+# Azure OpenAI API KEY...
+azure_api_type ='azure'
+azure_api_base ='https://XXXXXXX.openai.azure.com/'
+azure_api_version ='2023-05-15'
+azure_api_key ='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+azure_api_engine ='<Azure OpenAI DeployName>'
