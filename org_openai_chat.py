a4f11e10310abf2677be2fb98cd587eef78e818e
Shuichi Kataoka <shu01k9@gmail.com>
2024-08-17 20:43:00 +0900

    madel change, gpt-4o-mini

--- org_openai_chat.py
+++ org_openai_chat.py
@@ -17,7 +17,8 @@ client = OpenAI(
 # response = openai.ChatCompletion.create(
 response = client.chat.completions.create(
   # engine=openaiapikey.azure_api_engine,
-  model="gpt-3.5-turbo",
+  #model="gpt-3.5-turbo",
+  model="gpt-4o-mini",
   messages = [{"role":"system","content":"You are an AI assistant that helps people find information."},
               {"role":"user","content":"日本で一番高い山は何ですか、二番目に高い山も教えてください？"}],
   temperature=0.7,
